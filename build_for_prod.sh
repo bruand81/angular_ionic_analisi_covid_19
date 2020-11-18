fc15b9d506c9be20fb336cbd83d340b00b2ecb51
bruand <andrea.bruno@antaresnet.org>
2020-11-18 10:07:00 +0100

    Aggiunto script per build del progetto in produzione

--- build_for_prod.sh
+++ build_for_prod.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env sh
 CUR_DIR=$PWD
 
+echo "Changing current dir to $(dirname "$0")"
 cd "$(dirname "$0")" || exit
+
+echo "Start building app"
 ionic build --prod -- --base-href /www/
+
+echo "Changing current dir to $(CUR_DIR)"
 cd $CUR_DIR || exit
