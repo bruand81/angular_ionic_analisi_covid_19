76a3e37241d83162e9772fcbc2f1808f279ac5ad
bruand <andrea.bruno@antaresnet.org>
2020-12-26 17:51:08 +0100

    Fix script di build

--- build_for_prod.sh
+++ build_for_prod.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 CUR_DIR=$PWD
 
+git pull
+
 echo "Changing current dir to $(dirname "$0")"
 cd "$(dirname "$0")" || exit
 
 echo "Start building app"
 ionic build --prod -- --base-href /www/
 
-echo "Changing current dir to $(CUR_DIR)"
+echo "Changing current dir to $CUR_DIR"
 cd $CUR_DIR || exit
